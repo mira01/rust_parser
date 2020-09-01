63beae46a3e49d91339d07ee24ea1961b1b55480
Miroslav Čech <miroslav.cech@gooddata.com>
2020-09-01 10:09:22 +0200

    implement parsing identiffiers; implement first version of pair combinator

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,39 @@ fn match_literal(expected: &'static str) -> impl Fn(&str) -> Result<(&str, ()), 
     }
 }
 
+fn identifier(input: &str) -> Result<(&str, String), &str>{
+    let mut matched = String::new();
+    let mut chars = input.chars();
 
+    match chars.next() {
+        Some(next) if next.is_alphabetic() => matched.push(next),
+        _ => return Err(input),
+    }
+
+    while let Some(next) = chars.next(){
+        if next.is_alphanumeric() || next == '-' {
+            matched.push(next);
+        } else {
+            break;
+        }
+    }
+
+    Ok((&input.strip_prefix(&matched).unwrap(), matched))
+}
+
+fn pair<P1, P2, R1, R2>(parser1: P1, parser2: P2) -> impl Fn(&str) -> Result<(&str, (R1, R2)), &str>
+where
+P1: Fn(&str) -> Result<(&str, R1), &str>,
+P2: Fn(&str) -> Result<(&str, R2), &str>,{
+    move |input| match parser1(input){
+        Ok((next_input, result1)) =>
+            match parser2(next_input) {
+                Ok((final_input, result2)) => Ok((final_input, (result1, result2))),
+                Err(err) => Err(input)
+        },
+        Err(err) => Err(err),
+    }
+}
 
 
 #[cfg(test)]
@@ -40,5 +72,23 @@ mod tests {
         assert_eq!(parse_ahoj("ahoj bobe"), Ok((" bobe", ())));
 
         assert_eq!(parse_ahoj("čau"), Err("čau"));
+    }
+
+    #[test]
+    fn parse_identifier(){
+        assert_eq!(super::identifier("id-enti-ffier"), Ok(("", "id-enti-ffier".into())) );
+        assert_eq!(super::identifier("r2-d2 and co"), Ok((" and co", "r2-d2".into())) );
+        assert_eq!(super::identifier("-not at all"), Err("-not at all") );
+    }
+
+    #[test]
+    fn pair_combinator(){
+        let tag_opener = super::pair(super::match_literal("<"), super::identifier);
+        assert_eq!(tag_opener("<my-first-element/>"),
+             Ok(("/>", ((), "my-first-element".to_string())))
+        );
+        assert_eq!(tag_opener("oops"), Err("oops"));
+        assert_eq!(tag_opener("<!oops"), Err("<!oops"));
+
     }
 }
